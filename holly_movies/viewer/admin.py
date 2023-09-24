97229b36f61a17356cb055765a4e4e1b460d8312
stanislaw <stanislaw.kochanski@protonmail.com>
2023-09-24 10:13:52 +0200

    feat: final commit

--- holly_movies/viewer/admin.py
+++ holly_movies/viewer/admin.py
@@ -3,13 +3,49 @@ from django.contrib import admin
 from .models import Movie, Genre, Link
 
 
-admin.site.register(Movie)
 admin.site.register(Genre)
-
-
-###
 admin.site.register(Link)
 
 
-# class LinkAdmin(admin.ModelAdmin):
-#     list_display_links = ['id', 'name']
+class MovieAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("None", {"fields": ["title", "created"]}),
+        (
+            "External Information",
+            {
+                "fields": ["genre", "released"],
+                "description": (
+                    "these fields are going to be filled with data parsed"
+                    "from external database"
+                ),
+            },
+        ),
+        (
+            "User Information",
+            {
+                "fields": ["rating", "description"],
+                "description": "These fields are intended to be filled in by our users.",
+            },
+        ),
+    ]
+
+    readonly_fields = ["created"]
+
+    @staticmethod
+    def released_year(obj):
+        return obj.released.year
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        queryset.update(description=None)
+
+    ordering = ["id"]
+    list_display = ["id", "title", "genre", "released_year"]
+    list_display_links = ["id", "title"]
+    list_per_page = 20
+    list_filter = ["genre"]
+    search_fields = ["title"]
+    actions = ["cleanup_description"]
+
+
+admin.site.register(Movie, MovieAdmin)
